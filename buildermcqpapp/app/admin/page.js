086232d68dbc02fa844a175b5e271dec1d9629f4
yash-mathur-26 <yash.mathur@tatvasoft.com>
2024-01-16 15:37:33 +0530

    Added Folder structure and Admin First Page

---
+++ buildermcqpapp/app/admin/page.js
@@ -0,0 +1,7 @@
+export default function AdminPanel(){
+    return(
+        <main style={{ color: 'white', textAlign: 'center' }}>
+            <h1>Admin Panel</h1>
+        </main>
+    )
+}
---
+++ buildermcqpapp/components/main-header/main-header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import classes from './main-header.module.css';
+export default function MainHeader(){
+    return (
+        <>
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    Builder Exam Portal
+                </Link>
+            </header>
+        </>
+    ) 
+}
