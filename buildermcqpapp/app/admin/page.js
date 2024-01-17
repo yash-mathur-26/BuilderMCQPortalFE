b3688fb198ac79f69d8c42738e1cc9df1b4ecda5
yash-mathur-26 <yash.mathur@tatvasoft.com>
2024-01-17 15:32:01 +0530

    CSS changes

--- buildermcqpapp/app/admin/page.js
+++ buildermcqpapp/app/admin/page.js
@@ -5,12 +5,9 @@ import Image from "next/image";
 export default function AdminPanel(){
     return(
     <div className="flex h-screen">
-        <main style={{ color: 'white', textAlign: 'center' }}>
-            <h1 className={classes.header}>Admin Panel</h1>
-        </main>
-    <div className="flex-1 bg-cover bg-center">
-        <Image src={Test} alt="Test image"/>
-    </div>
+        <div className="flex-1 bg-cover bg-center">
+            <Image src={Test} className={classes.test} alt="Test image"/>
+        </div>
     <div className="flex-1 flex justify-center items-center">
         <AdminLogin/>
     </div>
