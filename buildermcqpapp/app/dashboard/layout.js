3204c392ed0255d70c659a0ac64a1208f33a574b
yash-mathur-26 <yash.mathur@tatvasoft.com>
2024-02-15 00:33:59 +0530

    Made Changes in Nav Bar For Users

--- buildermcqpapp/app/dashboard/layout.js
+++ buildermcqpapp/app/dashboard/layout.js
@@ -1,7 +1,5 @@
 "use client";
 import React from "react";
-import Logo from "../../components/logo/logo";
-import Link from "next/link";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
@@ -20,39 +18,6 @@ function Layout({ children }) {
   };
   return (
     <>
-      <header className="w-[100%] h-4 bg-[var(--color-grey-900)] flex justify-between items-center py-[5%]">
-        <Link href="/dashboard">
-          <Logo />
-        </Link>
-        <nav>
-          <ul className="list-none flex items-baseline m-0 pl-1">
-            <li className="pr-4">
-              <Link
-                href="/exams"
-                className="hover:text-gray-400 active:text-gray-400"
-              >
-                Exams
-              </Link>
-            </li>
-            <li className="pr-4">
-              <Link
-                href="/profile"
-                className="hover:text-gray-400 active:text-gray-400"
-              >
-                Profile
-              </Link>
-            </li>
-            <li className="pr-4">
-              <button
-                onClick={handleLogout}
-                className="bg-red-500 text-white p-2 rounded-md w-full"
-              >
-                Logout
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </header>
       {children}
     </>
   );
--- buildermcqpapp/app/dashboard/page.js
+++ buildermcqpapp/app/dashboard/page.js
@@ -1,3 +1,8 @@
-export default function Dashboard() {
-  return <>hello</>;
+import NavBarLinks from "@/components/user-nav-bar/user-nav-bar";
+export default function AdminDashboard() {
+  return (
+    <div className="min-h-screen">
+      <NavBarLinks />
+    </div>
+  );
 }
