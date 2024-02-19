e919823a03f6b104b5f309c44882c8eba3056353
yash-mathur-26 <yash.mathur@tatvasoft.com>
2024-02-19 15:51:59 +0530

    User Dashboard

--- buildermcqpapp/components/user-exam-dashboard/user-exam-dashboard.js
+++ buildermcqpapp/components/user-exam-dashboard/user-exam-dashboard.js
@@ -1,27 +1,22 @@
-import { getExams } from "@/lib/exam";
-
+import { getExams,getExamNameTechnologies } from "@/lib/exam";
+import Link from "next/link"; 
 export default async function UserExamDashboard(){
-    const examData = await getExams();
-    console.log("Exam Data on PAge", examData);
-    const exams = [
-        { id: 'technology1', title: 'Technology 1', questions: 20, status: 'idle' },
-        { id: 'technology2', title: 'Technology 2', questions: 15, status: 'completed' },
-        // Add more exams as needed
-        ];
+    const exam = await getExams();
+    const examName = await getExamNameTechnologies();
+    
     return (
             <div className="flex flex-wrap justify-around">
-            {exams.map((exam) => (
             <div key={exam.id} className="w-64 h-48 m-4 p-4 rounded-md shadow-md transition-transform transform bg-white hover:bg-gray-200">
-                <h2 className="text-xl font-bold mb-2">{exam.title}</h2>
-                <p className="text-gray-600 mb-2">Number of Questions: {exam.questions}</p>
-                {exam.status === 'idle' && (
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
-                >
-                    Start Exam
-                </button>
+                <h2 className="text-xl font-bold mb-2">{examName}</h2>
+                <p className="text-gray-600 mb-2">Number of Questions: {exam.score}</p>
+                <p className="text-gray-600 mb-2">Exam duration: {exam.duration}</p>
+                
+                {exam.isActive === true && (
+                        <Link href={`/dashboard/exam/${examName}`} className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
+                            Start Exam
+                        </Link>
                 )}
-                {exam.status === 'completed' && (
+                {exam.isActive === false && (
                 <button
                     className="bg-gray-400 text-gray-700 px-4 py-2 rounded cursor-not-allowed"
                     disabled
@@ -30,7 +25,6 @@ export default async function UserExamDashboard(){
                 </button>
                 )}
             </div>
-            ))}
         </div>
     )
 }
--- buildermcqpapp/lib/exam.js
+++ buildermcqpapp/lib/exam.js
@@ -1,9 +1,28 @@
 'use server';
 import { cookies } from "next/headers";
 
-export async function getExamQuestions(id) {
+export async function getExamQuestions() {
+  const techId = JSON.parse(cookies().get('userDataCookie').value)?.technology?.id;
   const response = await fetch(
-    `http://localhost:8000/api/questions/generate-test?technology=${id}`,
+    `http://localhost:8000/api/questions?technology=${techId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: cookies().get("userSession")?.value,
+      },
+    }
+  );
+  const data = await response.json();
+  console.log("Questions",data.data);
+  return data.data;
+}
+
+export async function getExams(){
+  'use server';
+  const techId = JSON.parse(cookies().get('userDataCookie').value)?.technology?.id;
+  const response = await fetch(
+    `http://localhost:8000/api/tests?technology=${techId}`,
     {
       method: "GET",
       headers: {
@@ -15,22 +34,17 @@ export async function getExamQuestions(id) {
   const data = await response.json();
   return data.data;
 }
-
-export async function getExams(){
-  'use server';
-  const techId = cookies().get("userDataCookie")?.value?.technology?.id;
-  const userId = cookies().get("userDataCookie")?.value?.id;
-  const response = await fetch(
-    `http://localhost:8000/api/tests?technology=${techId}&userId=${userId}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        token: cookies().get("userSession")?.value,
-      },
-    }
-  );
+export async function getExamNameTechnologies(){
+  const response = await fetch('http://localhost:8000/api/technologies',{
+      method: 'GET',
+      headers:{
+          'Content-Type': 'application/json',
+          'token': cookies().get('userSession')?.value
+      }
+  });
   const data = await response.json();
-  console.log("Exam Data",data)
-  return data.data;
+  const techData = data.data;  
+  const techId = JSON.parse(cookies().get('userDataCookie').value)?.technology?.id;
+  const idData = techData.filter((row)=>{ if(row.id == techId) return row});
+  return idData[0].name;
 }
